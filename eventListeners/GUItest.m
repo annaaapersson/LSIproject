7b5263fbf3ba31293b19f4a406653c80080a02b3
PerW <perjo768@student.liu.se>
2017-10-25 17:21:33 +0200

    Added ROI in GUI

--- eventListeners/GUItest.m
+++ eventListeners/GUItest.m
@@ -22,7 +22,7 @@ function varargout = GUItest(varargin)
 
 % Edit the above text to modify the response to help GUItest
 
-% Last Modified by GUIDE v2.5 24-Oct-2017 11:40:24
+% Last Modified by GUIDE v2.5 25-Oct-2017 17:13:54
 
 % Begin initialization code - DO NOT EDIT
 gui_Singleton = 1;
@@ -52,9 +52,12 @@ function GUItest_OpeningFcn(hObject, eventdata, handles, varargin)
 % handles    structure with handles and user data (see GUIDATA)
 % varargin   command line arguments to GUItest (see VARARGIN)
 
+% Create user
+per = user;
+
 % Choose default command line output for GUItest
 handles.output = hObject;
-
+                                 
 % Update handles structure
 guidata(hObject, handles);
 
@@ -78,3 +81,68 @@ function showImage_Callback(hObject, eventdata, handles)
 % hObject    handle to showImage (see GCBO)
 % eventdata  reserved - to be defined in a future version of MATLAB
 % handles    structure with handles and user data (see GUIDATA)
+
+a=imread('liftingbody.png');
+I = imshow(a);
+drawnow
+ROI_main(1:size(a,1),1:size(a,2))=0;
+ROI_main=uint8(ROI_main);
+for i=1:2
+      e = imrect(gca,[]);
+      BW = createMask(e,I);
+      ROI = a;
+       ROI(BW == 0) = [];     
+%       ROI(BW == 0) = 0;
+%       ROI_main=(ROI_main .* uint8(~BW)) + ROI;
+%       figure(2)
+%       imshow (ROI_main);
+%       drawnow
+%       figure(1)
+      valueROI = mean2(ROI)
+end
+valueROI = per.ROI1
+
+
+% --- Executes on selection change in popupmenu1.
+function popupmenu1_Callback(hObject, eventdata, handles)
+% hObject    handle to popupmenu1 (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+
+% Hints: contents = cellstr(get(hObject,'String')) returns popupmenu1 contents as cell array
+%        contents{get(hObject,'Value')} returns selected item from popupmenu1
+
+
+% --- Executes during object creation, after setting all properties.
+function popupmenu1_CreateFcn(hObject, eventdata, handles)
+% hObject    handle to popupmenu1 (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    empty - handles not created until after all CreateFcns called
+
+% Hint: popupmenu controls usually have a white background on Windows.
+%       See ISPC and COMPUTER.
+if ispc && isequal(get(hObject,'BackgroundColor'), get(0,'defaultUicontrolBackgroundColor'))
+    set(hObject,'BackgroundColor','white');
+end
+
+
+% --- Executes on slider movement.
+function slider2_Callback(hObject, eventdata, handles)
+% hObject    handle to slider2 (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+
+% Hints: get(hObject,'Value') returns position of slider
+%        get(hObject,'Min') and get(hObject,'Max') to determine range of slider
+
+
+% --- Executes during object creation, after setting all properties.
+function slider2_CreateFcn(hObject, eventdata, handles)
+% hObject    handle to slider2 (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    empty - handles not created until after all CreateFcns called
+
+% Hint: slider controls usually have a light gray background.
+if isequal(get(hObject,'BackgroundColor'), get(0,'defaultUicontrolBackgroundColor'))
+    set(hObject,'BackgroundColor',[.9 .9 .9]);
+end
---
+++ eventListeners/user.m
@@ -0,0 +1,10 @@
+classdef user
+    properties
+        ROI1
+        ROI2
+        userGain
+        userExposureTime
+    end
+    methods
+    end
+end
