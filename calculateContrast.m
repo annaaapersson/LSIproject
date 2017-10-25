004a2242df77fa627f28353a0363628230c9e57d
annaaapersson <32268772+annaaapersson@users.noreply.github.com>
2017-10-25 17:33:13 +0200

    Added some comments

--- calculateContrast.m
+++ calculateContrast.m
@@ -1,6 +1,11 @@
 clear
 
-laserWhite = mat2gray(single(imread('laser_only_6.bmp')));
+% Ambient light correction
+% ambient_light = imread('nolaser_white_anna.jpg');
+% raw_image = imread('laser_white_anna.jpg');
+
+%
+laserWhite = mat2gray(single(imread('paper_fast.jpg')));
 kernelSize = 5;
 rowCounter = floor(size(laserWhite,1)/kernelSize);
 colCounter = floor(size(laserWhite,2)/kernelSize);
@@ -10,14 +15,17 @@ meanImage = [];
 %%
 for row = 1:kernelSize:(rowCounter-1)*kernelSize+1
     for col = 1:kernelSize:(colCounter-1)*kernelSize+1
-       currentLocationMean = sum(sum(laserWhite(row:row+kernelSize-1,col:col+kernelSize-1)))/kernelSize^2;
-       meanImage(row:row+kernelSize-1,col:col+kernelSize-1) = currentLocationMean;
-       if currentLocationMean < 0.2
-           contrastImage(row:row+kernelSize-1,col:col+kernelSize-1) = 1;
+        %Calculate mean intensity at current location
+       currentLocationMean = sum(sum(laserWhite(row:row+kernelSize-1,col:col+kernelSize-1)))/kernelSize^2; 
+       %meanImage(row:row+kernelSize-1,col:col+kernelSize-1) = currentLocationMean;
+       if currentLocationMean < 0.2 %Threshold for the areas outside the hand to speed up the calculation
+           contrastImage(row:row+kernelSize-1,col:col+kernelSize-1) = 1; %Set the contrast to 1 for these areas
        else
            currentLocationSquared = laserWhite(row:row+kernelSize-1,col:col+kernelSize-1).^2;
+           %Calculate variance at current location
            varianceLocation = sum(sum(currentLocationSquared))/kernelSize^2 - currentLocationMean^2;
-           contrastLocation = sqrt(varianceLocation)/currentLocationMean;       
+           contrastLocation = sqrt(varianceLocation)/currentLocationMean; %Contrast at current location
+           %Save the contrast values in the new contrast image
            contrastImage(row:row+kernelSize-1,col:col+kernelSize-1) = contrastLocation;
        end
     end
@@ -25,10 +33,10 @@ end
 
 
 %%
-figure(1)
-imshow(8*contrastImage);
+% figure(1)
+% imshow(15*contrastImage);
 
-figure(3)
-imshow(-1*8*contrastImage+max(max(contrastImage)), 'Colormap', jet(255));
+figure(1)
+imshow(-1*4*contrastImage+max(max(contrastImage)), 'Colormap', jet(255));
 
     
