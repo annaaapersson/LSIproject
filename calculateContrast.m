8700912170343eb722f032456657de79dcd2d786
annaaapersson <32268772+annaaapersson@users.noreply.github.com>
2017-10-26 10:21:26 +0200

    Latest changes
    Added correction for max/min contrast

--- calculateContrast.m
+++ calculateContrast.m
@@ -1,11 +1,11 @@
 clear
 
-% Ambient light correction
+% -----  Ambient light correction
 % ambient_light = imread('nolaser_white_anna.jpg');
 % raw_image = imread('laser_white_anna.jpg');
 
 %
-laserWhite = mat2gray(single(imread('paper_fast.jpg')));
+laserWhite = single(imread('laser_dark_per_efter.jpg'));
 kernelSize = 5;
 rowCounter = floor(size(laserWhite,1)/kernelSize);
 colCounter = floor(size(laserWhite,2)/kernelSize);
@@ -26,17 +26,19 @@ for row = 1:kernelSize:(rowCounter-1)*kernelSize+1
            varianceLocation = sum(sum(currentLocationSquared))/kernelSize^2 - currentLocationMean^2;
            contrastLocation = sqrt(varianceLocation)/currentLocationMean; %Contrast at current location
            %Save the contrast values in the new contrast image
-           contrastImage(row:row+kernelSize-1,col:col+kernelSize-1) = contrastLocation;
+           contrastImage(row:row+kernelSize-1,col:col+kernelSize-1) = (contrastLocation-0.0356)/(0.3969-0.0356);
        end
     end
 end
-
-
+% finalContrastImage=((contrastLocation-0.0356)/(0.3969-0.0356));
 %%
-% figure(1)
-% imshow(15*contrastImage);
-
 figure(1)
-imshow(-1*4*contrastImage+max(max(contrastImage)), 'Colormap', jet(255));
+imshow(contrastImage);
+% figure(2)
+% imshow(finalContrastImage);
+%%
+contrastImage = mat2gray(contrastImage);
+figure(2)
+imshow(-1*15*contrastImage+max(max(contrastImage)), 'Colormap', jet(255));
 
     
