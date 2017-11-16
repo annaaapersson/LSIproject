2bfa63a7421251ba22991723a49ffc07a4612c7d
linagunnarsson <lingu459@student.liu.se>
2017-11-16 12:23:26 +0100

    Added classes

---
+++ Classes/imageWindow.m
@@ -0,0 +1,27 @@
+classdef imageWindow < handle
+%classdef (ConstructOnLoad) imageWindow < event.EventData
+    % Class for image windows 
+    properties
+        State = false;
+    end
+    events
+        processImageEvent
+    end
+    methods
+        function activate(obj, handles)
+            if true ~= obj.State
+                obj.State = true;
+                eventData = engineDataImageProcessing(handles.settings.kernelSize,...
+                    handles.camera.theCamera, handles.laser);
+                notify(obj, 'processImageEvent', eventData)
+            end
+        end
+         function deactivate(obj)
+            if false ~= obj.State
+                obj.State = false;
+                %notify(obj,'handleImageWindow');
+            end
+         end
+    end
+end
+
---
+++ Classes/respondToImageProcessing.m
@@ -0,0 +1,49 @@
+classdef respondToImageProcessing < handle 
+    properties
+        contrastImage;
+    end
+    methods
+        function obj = respondToImageProcessing(imageWindow)
+            addlistener(imageWindow,'processImageEvent',@respondToImageProcessing.handleEvnt);
+        end
+    end
+    methods (Static)
+        function handleEvnt(src,ed)
+            kernelSize = ed.kernelSize;
+            camera = ed.camera;
+            laser = ed.laser;
+            while (src.State == true)
+                %% If using webcam
+                imageNoLaser = snapshot(camera);
+                % Turn on laser
+                imageLaser = snapshot(camera);
+                % Turn off laser
+                %% If using pointgrey camera and laser:
+%                 start(camera)
+%                 trigger(camera); % If choosing manual trigger option
+%                 imageNoLaser = getdata(camera);
+%                 laser.start; % Turn on laser
+%                 start(camera);
+%                 trigger(camera); % If choosing manual trigger option
+%                 imageLaser = getdata(camera);
+%                 laser.stop; % Turn off laser
+                %% Image processing
+                %handles.settings.kernelSize
+                ambientLightCorrectedImage = imageLaser - imageNoLaser;
+                %     contrastImage = calculateContrastPreMatrix(ambientLightCorrectedImage,...
+                %         handles.settings.kernelSize);
+                contrastImage = calculateContrastNewSumMinimize(kernelSize,...
+                    ambientLightCorrectedImage);
+                imshow(contrastImage, 'Colormap', jet(255));
+                pause(0.01) % Neccessary in order to let other GUI be activated
+            end
+            %h = get('Name' , 'handles');
+            %kernelSizeInListener = h.settings.kernelSize
+            %while src.State == true
+            %handlesFromGUI = guidata(hObject);
+            %h = gcbo
+            %kernelSizeValue = h.settings.kernelSize
+            %end
+        end
+    end
+end
