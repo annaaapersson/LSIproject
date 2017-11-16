cbc4fba17ba1353e6ba30460e8abb43b017ae6a1
Onkelowa <32732228+Onkelowa@users.noreply.github.com>
2017-11-16 10:26:25 +0100

    Updated GUI
    • Some cosmetics have to be still done in terms of button size/ icons and representation

--- GUI/main.m
+++ GUI/main.m
@@ -5,7 +5,9 @@ function main()
         datas = struct;         % structure which stores all data
         
         LaserStarInc;           % calls gui function
- 
-% callback functions for the GUI
+        
+        
+        
+% callback functions for the GUI are stored in function files
 %##########################################################################
 
