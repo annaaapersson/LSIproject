5237a0c526d677adf8a70cb6378a8776318d2a25
linagunnarsson <lingu459@student.liu.se>
2017-11-16 12:06:09 +0100

    Move main one step up

---
+++ main.m
@@ -0,0 +1,18 @@
+function main()
+    close all
+    clear all
+    
+        handles = struct;       % structure which stores all objects
+        datas = struct;         % structure which stores all data
+        
+        %testFolder = addpath('testFolder');
+        %load hello.m;
+        hello();
+        
+        %LaserStarInc;           % calls gui function
+        
+        
+        
+% callback functions for the GUI are stored in function files
+%##########################################################################
+
