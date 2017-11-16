d79305234ad476f0e1c7d38e98c7dbc3abfa38e4
Onkelowa <32732228+Onkelowa@users.noreply.github.com>
2017-11-16 15:29:22 +0100

    Added video stream to settingsButton_Callback

--- GUI/settingsButton_Callback.m
+++ GUI/settingsButton_Callback.m
@@ -1,5 +1,5 @@
 function settingsButton_Callback(hObject, eventdata, handles)
-
+%% GUI
 % toggle off all panel which are not applicable for settings
 handles.measurePanel.Visible = 'off';
 handles.historyPanel.Visible = 'off';
@@ -12,4 +12,7 @@ handles.auxillaryPanel.Position(2) = 507/720;
 % toggle the visibility of the dedicated planes on
 handles.settingsPanel.Visible = 'on'; 
 handles.videoPanel.Visible = 'on';
+
+%% Functionallity 
+handles.LSIimageWindow.activate(handles);
 end
