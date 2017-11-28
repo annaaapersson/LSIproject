39115038fe97de640182a117d492cd4d5ee186ea
linagunnarsson <lingu459@student.liu.se>
2017-11-28 11:37:13 +0100

    Delete scriptForEventsListeners.m
    Removal of unused code - to get back storage space

--- eventListeners/scriptForEventsListeners.m
+++
@@ -1,9 +0,0 @@
-% X = 1:10;
-% Y = 1:10;
-% data = struct('XData', X, 'YData', Y);
-% eventData = MyEventData(data);
-% l = addlistener(a, 'MyEvent', @(evtSrc,evtData)dsip(evtData.Data));
-
-M = myClass;
-myListener = addlistener(M, 'myEvent', @myListenerCallback);
-getDataAndNotify(M)
