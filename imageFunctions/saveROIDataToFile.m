8fed8710612134e9be586ce4a2e42b2ef31091e1
linagunnarsson <lingu459@student.liu.se>
2017-12-06 14:33:48 +0100

    Add fkn hat saves ROIData to local folder ROIData

---
+++ imageFunctions/saveROIDataToFile.m
@@ -0,0 +1,12 @@
+function [] = saveROIDataToFile(handles, patientID, timestamp)
+% Saves ROI data to the ROIData folder
+
+ROI.Vector1 = handles.plotObject.valuesAllROI1;
+ROI.Vector2 = handles.plotObject.valuesAllROI2;
+ROI.time = handles.plotObject.counter;
+ROI.imageTimes = handles.plotObject.imageTimes;
+
+save(createFullDestinationROIData(patientID, timestamp), '-struct', 'ROI');
+
+end
+
