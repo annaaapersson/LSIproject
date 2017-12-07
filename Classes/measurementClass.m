975c5f4cedfc74765566541b3d88646dc30b372c
Onkelowa <32732228+Onkelowa@users.noreply.github.com>
2017-12-07 16:38:37 +0100

    Golden master alpha 0.9.2.2

--- Classes/measurementClass.m
+++ Classes/measurementClass.m
@@ -44,7 +44,7 @@ classdef measurementClass < handle
             % Input 'patientID', 'timestamp' and 'imageType' of type string
             % 'imageType' can be baseline, min, max, ROI1 and ROI2.
             addpath(genpath(pwd));
-            imageFilename = getImageFilename(patientID, timestamp, imageType)
+            imageFilename = getImageFilename(patientID, timestamp, imageType);
             image = imread(imageFilename);
          end
          
